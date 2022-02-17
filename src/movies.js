5a9750c75a7dc2bd886f0e40da04f4b801539864
Lysann Kuettner <ly.sann@live.de>
2022-02-17 21:27:00 +0100

    lab done

--- src/movies.js
+++ src/movies.js
@@ -1,22 +1,47 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(allMovies) {
+  let allDirectors = allMovies.map(function (movie) {
+    return movie.director;
+  });
+  return allDirectors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  let spielbergDramaMovies = movies.filter((singleMovie) => {
+  return singleMovie.director === "Steven Spielberg" && singleMovie.genre.includes("Drama")
+  });
+  return spielbergDramaMovies.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(scores) {
+  let result = scores.reduce((total, score) => total + score) / scores.length;
+  return result;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  movies.sort((a, b) => {
+    return a.year - b.year;
+  });
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(allMovies) {
+ const order = JSON.parse(JSON.stringify(allMovies)).sort((a, b) => {
+    if (a.title === b.title) return 0;
+    if (a.title > b.title) {
+      return -1;
+    }
+    return 1;
+}); 
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
